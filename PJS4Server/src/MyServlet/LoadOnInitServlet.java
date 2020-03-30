48689461851e7bd4c33428b9e21120f83cd1fb76
Clemen75 <clementboutheg@gmail.com>
2020-03-30 17:19:56 +0200

    Modif LoadOnInitSrvlet

--- PJS4Server/src/MyServlet/LoadOnInitServlet.java
+++ PJS4Server/src/MyServlet/LoadOnInitServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class LoadOnInitServlet
  */
+@WebServlet (urlPatterns="/initializeResources", loadOnStartup=1)
 public class LoadOnInitServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -20,6 +22,7 @@ public class LoadOnInitServlet extends HttpServlet {
 	@Override
 	public void init(ServletConfig arg0) throws ServletException {
 		super.init(arg0);
+		System.out.println("rrr");
 		
 		
 		try {
